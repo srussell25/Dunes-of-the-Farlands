eab4db0334cd11e4297117021bb900ade598f9d8
Lremon116 <lremon1@lsu.edu>
2023-10-05 21:44:01 -0500

    added mock commands and comments

--- code/objectactions.h
+++ code/objectactions.h
@@ -2,6 +2,23 @@
 #include <iostream>
 
 // Generic starter code for each file
+
+/*the functions should check if you have the right check to see if you can preform the action for example
+if you try to take a item you need to make sure that you can reach it (if there is a thing in the way) or even
+if your in the same location as the item or in some cases if you even have the item to use in the first place */
+void take (){
+//if in_location = true {take item} or if blocked by something check for that, block the user and say somthing like "can't take that right now"
+}
+void use (){
+// check if they have_item and if the are in_location this is similar to the attack command 
+}
+void move (){
+    /*change the users location based on where they are. if they are connected to the next place from where they are 
+    move the player though the player_location variale or though bools turning on and off(this method can take a long time to set up
+    if we give the player a way to move back and forth between locations). if they can't go to a location because its blocked check for that*/
+    
+}
+
 void function4()
 {
     std::cout << "Function not yet implemented\n";
