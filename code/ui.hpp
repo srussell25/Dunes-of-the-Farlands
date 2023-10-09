495f036906fd061d339264eab89b430a974dbe0a
Connor Morris <cmorr78@lsu.edu>
2023-10-08 19:50:38 -0500

    Deleted code that needs rewrite, fixed player_info

--- code/ui.hpp
+++ code/ui.hpp
@@ -4,69 +4,53 @@
 #include <string>
 #include <stdio.h>
 
-// method for prompting and reading user input
+// Method for prompting and reading user input
 std::string getInput()
 {
     std::string temp;
-    // prompts for user input without skipping a line
+    // Prompts for user input without skipping a line
     std::cout<<"> ";
-    // reads user input into temp until a newline character is reached
+    // Reads user input into temp until a newline character is reached
     getline(std::cin,temp);
-    // returns the player input
+    // Returns the player input
     return temp;
 }
 
-// function for returning npc dialogue to the screen
+// Function for returning npc dialogue to the screen
 void npcText(std::string dialogue, std::string npcName, std::string format = ": ")
 {
     int wrap = 100;
-    // default formatting takes the style [Name: dialogue], however the format field was included
+    // Default formatting takes the style [Name: dialogue], however the format field was included
     // as an optional argument in case specific NPC entities should have different dialogue style
     if (dialogue.length() > wrap)
     {
-
+        // unimplemented
     }
     std::string output = npcName+format+dialogue;
     std::cout<<output<<std::endl;
 }
 
-// method for returning all non dialogue information to the screen
+// Method for returning all non-dialogue information to the screen
 void narrator(std::string Text)
 {
     std::cout<<"["+Text+"]"<<std::endl;
 }
 
-// TODO: make sure this actually functions lol
+// TODO: Test this function properly
 bool exitSeq()
 {
     std::cout<<"Are you sure? (Y/N)"<<std::endl;
-    // creates a loop that continuously gets the next character until it finds either a Y or N reply
+    // Creates a loop that continuously gets the next character until it finds either a Y or N reply
     while (true)
     {
-    char reply = getchar();
-    if (reply == 'y' || reply == 'Y')
-        return true;
-    else if (reply == 'n' || reply == 'N')
-        return false;
-    }
-}
-
-// TODO: decide if this function is needed
-void pauseGame()
-{
-    std::cout<<"1] Resume"<<std::endl<<"2] Save Game"<<std::endl<<"3] Save and Exit"<<std::endl;
-    char input = getchar();
-    switch (input)
-    {
-        case '1':
-            break;
-        case '2':
-            // save function here
-            break;
-        case '3':
-            // save function here
-            if (exitSeq() == true)
-                exit(0);
-            break; // should be unneccesary, but better safe than sorry
+        char reply = getchar();
+        if (reply == 'y' || reply == 'Y')
+        {
+            return true;
+        }
+        else if (reply == 'n' || reply == 'N')
+        {    
+            return false;
+        }
     }
 }
