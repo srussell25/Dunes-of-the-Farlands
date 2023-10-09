f0003e1d8032c8f0f77ff0443a20b817ab4ce178
Lremon116 <lremon1@lsu.edu>
2023-10-09 16:08:49 -0500

    edited game objectactions

--- code/objectactions.hpp
+++ code/objectactions.hpp
@@ -1,5 +1,6 @@
 #ifndef OBJECTACTIONS_HPP
 #define OBJECTACTIONS_HPP
+#endif
 
 // This function will take in an action as a string and the current 
 // game object being called upon; it will then check the name of the 
@@ -9,10 +10,35 @@
 std::string main_action(std::string act, game_object obj) 
 {
     std::string result;
-
+    if (act == "use")
+    { 
+            result= use(obj);
+    }
+    else (act == "take")
+    {
+        result = take(obj);
+    }
     // code goes here
 
     return result;
 }
 
-#endif
+
+
+std::string use(game_object obj)
+{
+    if (obj.get_object_name() == "key")
+    {
+        return "you used the key";
+    }
+    
+    
+}
+std::string take(game_object obj)
+{
+    if (obj.get_object_name() == "key")
+    {
+        player.add_item(obj);
+        return "you added the key to your inventory";
+    }
+}
