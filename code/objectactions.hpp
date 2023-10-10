3ac5215988c205104ceee93a18bd11f39551972f
Lremon116 <lremon1@lsu.edu>
2023-10-09 21:27:56 -0500

    added actions

--- code/objectactions.hpp
+++ code/objectactions.hpp
@@ -12,7 +12,7 @@ std::string use(game_object obj, player_info &playerChar)
     else if (obj.get_object_name() == "drink"){
         if (obj.get_object_flag("at_object") == "at_object")
         {
-            playerChar.remove_item
+            playerChar.remove_item(obj);
             return "man that drink was pretty good.";
         }
         else
@@ -51,12 +51,25 @@ std::string go_to(game_object obj, player_info &playerChar)
     {
        if (playerChar.get_location().get_object_name() == "gameStart")
        {
-        playerChar.set_location(obj) = oasis; ;
+        playerChar.set_location(obj) = oasis; 
         return "you got to the oasis. when you make it there a frog jumps onto your legs and poisons you,"
          "you die in seconds.";
          playerChar.set_player_state(false);
        }
     }
+    else if (obj.get_object_name() == "abandonedTown"){
+        if (playerChar.get_location().get_object_name() == "gameStart")
+       {
+        playerChar.set_location(obj) = abandonedTown; 
+        oldLady.add_object_flag("nearCharacter");
+        abandonedTown.add_object_flag("at_town");
+        gameStart.remove_object_flag("at_start");
+        return "You arrive at the town named Nekhem. The town isnt necessarily abandoned, but its overrun by thugs and bandits."
+         "The walls are broken, and people have malicious looks on their faces. there is a tavern called the Sand Dune Saloon. there is also a old lady who seems to be struggling. what do you do?";
+         
+       }
+    }
+
     else if (obj.get_object_name() == "tavern")
     {
         if (playerChar.get_location().get_object_name() == "abandonedTown"){
@@ -65,15 +78,25 @@ std::string go_to(game_object obj, player_info &playerChar)
         barKeep.add_object_flag("nearCharacter");
         bandit.add_object_flag("nearCharacter");
         tavern.add_object_flag("at_tavern");
-        drink.add_object_flag("at_object")
-    return "you entered into the taver. it is full with towns people. you go up to the bar to ask for directions."
+        drink.add_object_flag("at_object");
+        abandonedTown.remove_object_flag("at_town");
+        oldLady.remove_object_flag("nearCharacter");
+        
+    return "you entered into the tavern. it is full with towns people. you go up to the bar to ask for directions."
     "unfortunalty as your walking up to the counter you step on you you accidentally step on someones foot "
     "they stand up and draw there swords. what do you do?";
         }
     return "";
 }
+}
 
-
+std::string talk_to(game_object obj, player_info &playerChar){
+        if(obj.get_object_flag("bartender") == "bartender" ||obj.get_object_flag("barkeep") == "barkeep" ){
+            if (obj.get_object_flag("nearCharacter") == "nearCharacter"){
+            return "hey im the bartender of the Sand Dune Saloon."
+         }
+        }
+}
 
 
 //if there is an object to look at, call this function for the description
@@ -95,7 +118,7 @@ std::string attack(game_object obj, player_info &playerChar){
     if(obj.get_object_type() == "character" && obj.get_object_flag("nearCharacter") == "nearCharacter"){
         if(obj.get_object_name() == "bandit"){
             bandit.remove_object_flag("is_alive");
-            return "You have slain the bandit with your sword.";
+            return "You have slay the bandit with your sword. no one in that Sand Dune Saloon bats a eye. the bartender bekends you over to talk.";
         }
         else if(obj.get_object_name() == "oldLady"){
             return "Interesting... ";
@@ -104,6 +127,7 @@ std::string attack(game_object obj, player_info &playerChar){
             return "He's a bar keep, have some self discipline";
         }
     }
+    return "";
 }
 
 //read things like books, notes, etc.
@@ -191,6 +215,10 @@ std::string main_action(std::string act, game_object obj, player_info &playerCha
     else if (act == "get")
     {
         result = get(obj, playerChar);
+    }
+    else if (act == "talk to")
+    {
+        result = talk_to(obj, playerChar);
     }
     else if (act == "grab")
     {
