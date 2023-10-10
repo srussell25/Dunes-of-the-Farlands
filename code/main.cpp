9a697d84eed884379ea4e59d2ff7a019e0ccfb59
Lremon116 <lremon1@lsu.edu>
2023-10-09 20:01:51 -0500

    added gameactions

--- code/main.cpp
+++ code/main.cpp
@@ -15,60 +15,60 @@
 
 int main()
 {
-    // Setting up all the game objects
-    initialize_game_objects();
-
-    // TODO: Check if the player is alive in the game loop; 
-    // if not (due to a gameplay event), re-instance 
-    // this variable and restart the game.
-    player_info player = player_info();
-    
     std::string inputText;
     std::string outputText;
     std::tuple<std::string, game_object> parserOutput;
     game_object emptyGameObj;
+    player_info player;
 
-    // Title card
-    std::cout << "DUNES OF THE FARLANDS" << std::endl << "=====================" << std::endl << std::endl;
-    std::cout << "Enter any key to start" << std::endl;
-
-    // Make the program wait until the user inputs any character
-    get_input();
-
+    // Main program loop
     while(true)
-    { 
-        // Get the player's current input
-        inputText = get_input();
-    
-        // Send input to parser
-        parserOutput = game_input_parser(inputText);
+    {
+        // Setting up all the game objects & player character
+        initialize_game_objects();
+        player = player_info();
 
-        // If there is invalid input, skip to next loop iteration
-        if (get<1>(parserOutput) == emptyGameObj) {
-            std::cout << "Invalid input; type 'HELP' for a list of all commands." << std::endl;
-            continue;
-        }
+        // Title card
+        std::cout << "DUNES OF THE FARLANDS" << std::endl << "=====================" << std::endl << std::endl;
+        std::cout << "Enter any key to start" << std::endl;
 
-        // Send currentAction & currentGameObject to mainAction, get output
-        outputText = main_action(get<0>(parserOutput), get<1>(parserOutput), player);
+        // Make the program wait until the user inputs any character
+        get_input();
 
-        // Output text to terminal
-        narrator(outputText);
-
-        /*
-        Exit program if player input is "EXIT"
-        if (input == "EXIT")
+        // Main gameplay loop; if player dies, break loop to restart
+        while(!player.get_player_state()) 
         {
-            if (exit_seq() == true)
+            // Get the player's current input
+            inputText = get_input();
+
+            // Send input to parser
+            parserOutput = game_input_parser(inputText);
+
+            // If there is invalid input, skip to next loop iteration
+            if (get<1>(parserOutput) == emptyGameObj) 
             {
-                exit(0);
+                std::cout << "Invalid input; type 'HELP' for a list of all commands." << std::endl;
+                continue;
             }
-            else
-            {
-                std::cin.ignore();
-            }
+
+            // Send currentAction & currentGameObject to mainAction, get output
+            outputText = main_action(get<0>(parserOutput), get<1>(parserOutput), player);
+
+            // Output text to terminal
+            narrator(outputText);
         }
-        */
+
+        // Game Over loop; if player answers no, quit the game
+        std::cout << std::endl << "Game Over!" << std::endl;
+        std::cin.ignore();
+        if (!exit_seq("Try again?"))
+        {
+            break;
+        }
+        else
+        {
+            std::cin.ignore();
+        }
     }
   
     return 0;
