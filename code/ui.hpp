d4b1d218e9d080eca5722a0c451c6d8b85649429
LaneDurst <ltdurst03@gmail.com>
2023-11-09 21:20:49 -0600

    Removed Unneccesary Includes

--- code/ui.hpp
+++ code/ui.hpp
@@ -1,16 +1,6 @@
 #ifndef UI_HPP
 #define UI_HPP
 
-
-//ftxui includes
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/dom/node.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <ftxui/screen/box.hpp>
-#include <ftxui/component/component.hpp>
-#include <ftxui/component/component_base.hpp>
-#include <ftxui/component/screen_interactive.hpp>
-
 #include <iostream>
 #include "classesmisc.hpp"
 
