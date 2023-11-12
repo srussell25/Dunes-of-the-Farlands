61334fc501a67f489962fbbb8c555708a44e95cd
Connor Morris <cmorr78@lsu.edu>
2023-11-12 13:29:47 -0600

    Changed runtests formatting, added some tests

--- code/runtests.cpp
+++ code/runtests.cpp
@@ -21,10 +21,14 @@ STUDENT_TEST("Check to see that find_object exhibits proper behavior.")
 {
     initialize_game_objects();
 
-    // Try to find game_object, check that the return values match expected behavior
     CHECK(find_object("game start") == specificvars::main_objects.at(0));
     CHECK_FALSE(find_object("tavern") == specificvars::empty_object);
     CHECK(find_object("fake object") == specificvars::empty_object);
+
+    // Try to find game_object(s) w/ synonyms for names
+    CHECK_FALSE(find_object("nekhem") == specificvars::empty_object);
+    CHECK_FALSE(find_object("lady") == specificvars::empty_object);
+    CHECK_FALSE(find_object("king") == specificvars::empty_object);
 }
 
 STUDENT_TEST("Check parser behavior with valid inputs.")
@@ -48,6 +52,9 @@ STUDENT_TEST("Check parser behavior with valid inputs.")
     CHECK(parser_output.second == specificvars::empty_object);
     parser_output = game_input_parser("exit");
     CHECK(parser_output.first == "exit");
+    CHECK(parser_output.second == specificvars::empty_object);
+    parser_output = game_input_parser("leave"); // Faux-regular command, is changed in main.cpp
+    CHECK(parser_output.first == "leave");
     CHECK(parser_output.second == specificvars::empty_object);
 
     // Check if regular commands are returned
@@ -144,18 +151,15 @@ STUDENT_TEST("Check to see that game_object flags can be retrieved, added, & rem
 {
     initialize_game_objects();
 
-    // Try to find game_object flags, check that the return values match expected behavior
-    CHECK_FALSE(find_object("game start").get_object_flag("temp_flag")); // empty flag map
-    CHECK(find_object("bandit").get_object_flag("is_alive")); // non-empty flag map
+    CHECK_FALSE(find_object("game start").get_object_flag("temp_flag")); // Empty flag map
+    CHECK(find_object("bandit").get_object_flag("is_alive")); // Non-empty flag map
 
-    // Try to add a flag to a game_object, check if successful
-    find_object("tavern").set_object_flag("temp_flag", true); // empty flag map
+    find_object("tavern").set_object_flag("temp_flag", true); // Empty flag map
     CHECK(find_object("tavern").get_object_flag("temp_flag"));
-    find_object("old lady").set_object_flag("temp_flag", true); // non-empty flag map
+    find_object("old lady").set_object_flag("temp_flag", true); // Non-empty flag map
     CHECK(find_object("old lady").get_object_flag("temp_flag"));
 
-    // Try to remove a flag from a game_object, check if successful
-    find_object("barkeep").remove_object_flag("is_alive"); // non-empty flag map
+    find_object("barkeep").remove_object_flag("is_alive"); // Non-empty flag map
     CHECK_FALSE(find_object("barkeep").get_object_flag("is_alive"));
 }
 
@@ -166,11 +170,9 @@ STUDENT_TEST("Flag Set Check #1: Attacking the bandit in the tavern.")
     player_info player = player_info("new");
     std::pair<std::string, std::reference_wrapper<game_object>> parser_output = {"", specificvars::empty_object};
 
-    // Check that the bandit is alive
-    CHECK(find_object("bandit").get_object_flag("is_alive"));
+    CHECK(find_object("bandit").get_object_flag("is_alive")); // Bandit is alive
 
-    // Simulate gameplay loop
-    parser_output = game_input_parser("go to abandoned town");
+    parser_output = game_input_parser("go to abandoned town"); // Simulate gameplay loop
     main_action(parser_output.first, (parser_output.second).get(), player);
     CHECK(player.get_player_loc() == "abandoned town");
 
@@ -180,9 +182,34 @@ STUDENT_TEST("Flag Set Check #1: Attacking the bandit in the tavern.")
 
     parser_output = game_input_parser("attack bandit");
     main_action(parser_output.first, (parser_output.second).get(), player);
+    CHECK_FALSE(find_object("bandit").get_object_flag("is_alive")); // Bandit is dead
+}
 
-    // Check that the bandit is dead
-    CHECK_FALSE(find_object("bandit").get_object_flag("is_alive"));
+STUDENT_TEST("Flag Set Check #2: Recognizing the old lady's status.")
+{
+    initialize_parser();
+    initialize_game_objects();
+    player_info player = player_info("new");
+    std::pair<std::string, std::reference_wrapper<game_object>> parser_output = {"", specificvars::empty_object};
+
+    CHECK(find_object("old lady").get_object_flag("is_alive")); // Old lady is alive & not known to be evil
+    CHECK_FALSE(find_object("old lady").get_object_flag("known_evil"));
+
+    parser_output = game_input_parser("go to abandoned town"); // Simulate gameplay loop
+    main_action(parser_output.first, (parser_output.second).get(), player);
+    CHECK(player.get_player_loc() == "abandoned town");
+
+    parser_output = game_input_parser("attack the old lady");
+    main_action(parser_output.first, (parser_output.second).get(), player);
+    CHECK(find_object("old lady").get_object_flag("is_alive")); // Can't be attacked yet
+
+    parser_output = game_input_parser("look at the old lady");
+    main_action(parser_output.first, (parser_output.second).get(), player);
+    CHECK(find_object("old lady").get_object_flag("known_evil")); // Now known to be evil
+
+    parser_output = game_input_parser("attack the old lady");
+    main_action(parser_output.first, (parser_output.second).get(), player);
+    CHECK_FALSE(find_object("old lady").get_object_flag("is_alive")); // Old lady is dead
 }
 
 STUDENT_TEST("Inventory Check #1: Taking the whiskey from the barkeep and drinking it.")
@@ -192,13 +219,11 @@ STUDENT_TEST("Inventory Check #1: Taking the whiskey from the barkeep and drinki
     player_info player = player_info("new");
     std::pair<std::string, std::reference_wrapper<game_object>> parser_output = {"", specificvars::empty_object};
 
-    // Check that the drink exists, is not in the player's inventory, & is located in the tavern
-    CHECK_FALSE(find_object("whiskey") == specificvars::empty_object);
-    CHECK_FALSE(player.get_inv_item("whiskey"));
-    CHECK(find_object("whiskey").get_object_loc() == "tavern");
+    CHECK_FALSE(find_object("whiskey") == specificvars::empty_object); // Drink exists
+    CHECK_FALSE(player.get_inv_item("whiskey")); // Is not in the player's inventory
+    CHECK(find_object("whiskey").get_object_loc() == "tavern"); // Is located in the tavern
 
-    // Simulate gameplay loop
-    parser_output = game_input_parser("go to abandoned town");
+    parser_output = game_input_parser("go to abandoned town"); // Simulate gameplay loop
     main_action(parser_output.first, (parser_output.second).get(), player);
     CHECK(player.get_player_loc() == "abandoned town");
 
@@ -212,23 +237,48 @@ STUDENT_TEST("Inventory Check #1: Taking the whiskey from the barkeep and drinki
     parser_output = game_input_parser("take whiskey");
     main_action(parser_output.first, (parser_output.second).get(), player);
 
-    // Check that the drink exists, is added to the player's inventory, & is not located in the tavern
-    CHECK_FALSE(find_object("whiskey") == specificvars::empty_object);
-    CHECK(player.get_inv_item("whiskey"));
-    CHECK_FALSE(find_object("whiskey").get_object_loc() == "tavern");
+    CHECK_FALSE(find_object("whiskey") == specificvars::empty_object); // Drink exists
+    CHECK(player.get_inv_item("whiskey")); // Is in the player's inventory
+    CHECK_FALSE(find_object("whiskey").get_object_loc() == "tavern"); // Is not located in the tavern
 
     parser_output = game_input_parser("drink whiskey");
     main_action(parser_output.first, (parser_output.second).get(), player);
 
-    // Check that the drink does not exist & is not in the player's inventory
-    CHECK(find_object("whiskey") == specificvars::empty_object);
-    CHECK_FALSE(player.get_inv_item("whiskey"));
-    CHECK_FALSE(find_object("whiskey").get_object_loc() == "tavern");
+    CHECK(find_object("whiskey") == specificvars::empty_object); // Drink doesn't exist
+    CHECK_FALSE(player.get_inv_item("whiskey")); // Is not in the player's inventory
 }
 
-//UI Method Tests//
+STUDENT_TEST("Movement Check #1: Going from the start of the game to the Farlands.")
+{
+    initialize_parser();
+    initialize_game_objects();
+    player_info player = player_info("new");
+    std::pair<std::string, std::reference_wrapper<game_object>> parser_output = {"", specificvars::empty_object};
 
-STUDENT_TEST("Word Breaker Test")
+    parser_output = game_input_parser("go to the abandoned town"); // Simulate gameplay loop
+    main_action(parser_output.first, (parser_output.second).get(), player);
+    CHECK(player.get_player_loc() == "abandoned town");
+
+    parser_output = game_input_parser("go to the saloon");
+    main_action(parser_output.first, (parser_output.second).get(), player);
+    CHECK(player.get_player_loc() == "tavern");
+
+    parser_output = game_input_parser("attack bandit");
+    main_action(parser_output.first, (parser_output.second).get(), player);
+
+    parser_output = game_input_parser("talk to the bartender");
+    main_action(parser_output.first, (parser_output.second).get(), player);
+    
+    parser_output = {"go", std::ref(find_object(player.get_player_loc_prev()))}; // Simulate 'leave' command within 'main.cpp'
+    main_action(parser_output.first, (parser_output.second).get(), player);
+    CHECK(player.get_player_loc() == "abandoned town");
+
+    parser_output = game_input_parser("go to the farlands");
+    main_action(parser_output.first, (parser_output.second).get(), player);
+    CHECK(player.get_player_loc() == "farlands");
+}
+
+STUDENT_TEST("Testing the 'word_breaker' function.")
 {
     std::vector<std::string> hello_world = {"Hello,", "World"};
     std::vector<std::string> empty_test = {""};
@@ -243,16 +293,16 @@ STUDENT_TEST("Word Breaker Test")
     CHECK(word_breaker("extra     spaces") == extra_spaces_test);
 }
 
-STUDENT_TEST("Center Text Test")
+STUDENT_TEST("Testing the 'center_text' function.")
 {
     CHECK(center_text("Hello, World", 14) == " Hello, World ");
     CHECK(center_text("Text is Too Large for given Line Length", 7) == "Text is Too Large for given Line Length");
-    CHECK(center_text("Uneven Line Length", 19) == "Uneven Line Length"); //center_displacement = 1/2 in this calc which should round down to 0 spaces added
+    CHECK(center_text("Uneven Line Length", 19) == "Uneven Line Length"); //center_displacement = 1/2, rounds down to 0 spaces added
     CHECK(center_text("", 14) == "              ");
     CHECK(center_text("Negative Line Length :(", -14) == "Negative Line Length :(");
 }
 
-STUDENT_TEST("Border Generation Test")
+STUDENT_TEST("Testing the 'generate_border' function.")
 {
     CHECK(generate_border(10) == "==========");
     CHECK(generate_border(0) == "");
